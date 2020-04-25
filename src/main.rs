0d0a97bdf2457e31a7d1c59f8c1d153296288399
Kat <10361870+katattakd@users.noreply.github.com>
2020-04-25 01:32:02 +0000

    Improved css handling

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@
 #![allow(clippy::cargo)]
 #![deny(clippy::all)]
 
-// TODO: Implement brotli and gzip compression.
-
 extern crate ammonia;
 extern crate comrak;
 extern crate exitcode;
 extern crate glob;
 extern crate hyperbuild;
 extern crate liquid;
+extern crate minifier;
 extern crate rayon;
 extern crate serde_derive;
 extern crate toml;
@@ -19,7 +18,7 @@ use glob::glob;
 use hyperbuild::hyperbuild;
 use rayon::prelude::*;
 use serde_derive::Deserialize;
-use std::{fs, fs::File, io::Write};
+use std::{fs, fs::File, io::Write, process::exit};
 
 #[derive(Deserialize)]
 struct Config {
@@ -54,9 +53,9 @@ struct Minifier {
 struct HTML {
 	append_doctype: bool,
 	append_viewport: bool,
+	append_css_link: bool,
 	custom_css: String,
-	custom_header_html: String,
-	custom_footer_html: String,
+	custom_html: String,
 }
 
 fn parse_to_html(mut markdown_input: String, config: &Config) -> Result<std::vec::Vec<u8>, Box<dyn std::error::Error>> {
@@ -106,7 +105,28 @@ fn parse_to_html(mut markdown_input: String, config: &Config) -> Result<std::vec
 	Ok(html_output)
 }
 
-fn load_parse_write(fpath: &std::path::Path, header: &[u8], footer: &[u8], config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+fn create_css_file(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+	let mut css = config.html.custom_css.to_owned();
+	if config.minifier.minify_custom {
+		match minifier::css::minify(&css) {
+			Ok(output) => {
+				css = output
+			},
+			Err(error_msg) => {
+				return Err(std::io::Error::new(
+					std::io::ErrorKind::Other, error_msg
+				).into())
+			}
+		}
+	}
+
+	let mut file = File::create("style.css")?;
+	file.write_all(css.as_bytes())?;
+
+	Ok(())
+}
+
+fn load_parse_write(fpath: &std::path::Path, custom_html: &[u8], config: &Config) -> Result<(), Box<dyn std::error::Error>> {
 	let markdown_input = fs::read_to_string(&fpath)?;
 	let html_output = parse_to_html(markdown_input, config)?;
 
@@ -115,21 +135,19 @@ fn load_parse_write(fpath: &std::path::Path, header: &[u8], footer: &[u8], confi
 		file.write_all(b"<!doctype html>")?;
 	}
 	if config.html.append_viewport {
-		//file.write_all(b"<meta name="viewport" content=\"width=device-width,initial-scale=1\">")?;
 		file.write_all(b"<meta name=viewport content=\"width=device-width,initial-scale=1\">")?;
 	}
-	if !config.html.custom_css.is_empty() {
+	if config.html.append_css_link {
+		file.write_all(b"<link rel=stylesheet href=style.css>")?;
+	} else if !config.html.custom_css.is_empty() {
 		file.write_all(b"<style>")?;
 		file.write_all(config.html.custom_css.as_bytes())?;
 		file.write_all(b"</style>")?;
 	}
-	if !header.is_empty() {
-		file.write_all(header)?;
+	if !custom_html.is_empty() {
+		file.write_all(custom_html)?;
 	}
 	file.write_all(&html_output)?;
-	if !footer.is_empty() {
-		file.write_all(footer)?;
-	}
 	Ok(())
 }
 
@@ -137,27 +155,27 @@ fn main() {
 	println!("Loading config...");
 	let config_input = fs::read_to_string("conf.toml").unwrap_or_else(|_| {
 		println!("Unable to read config file!");
-		std::process::exit(exitcode::NOINPUT);
+		exit(exitcode::NOINPUT);
 	});
 	let config: Config = toml::from_str(&config_input).unwrap_or_else(|err| {
 		println!("Unable to parse config file! Additional info below:\n{:#?}", err);
-		std::process::exit(exitcode::CONFIG);
+		exit(exitcode::CONFIG);
 	});
 
-	let mut custom_header = config.html.custom_header_html.as_bytes().to_vec();
-	let mut custom_footer = config.html.custom_footer_html.as_bytes().to_vec();
+	let mut custom_html = config.html.custom_html.as_bytes().to_vec();
 	if config.minifier.minify_custom {
-		let header_len = hyperbuild(&mut custom_header).unwrap_or_else(|err| {
+		let header_len = hyperbuild(&mut custom_html).unwrap_or_else(|err| {
 			println!("Unable to minify html.custom_header_html! Additional info below:\n{:#?}", err);
-			std::process::exit(exitcode::CONFIG);
+			exit(exitcode::CONFIG);
 		});
-		custom_header.truncate(header_len);
+		custom_html.truncate(header_len);
+	}
 
-		let footer_len = hyperbuild(&mut custom_footer).unwrap_or_else(|err| {
-			println!("Unable to minify html.custom_footer_html! Additional info below:\n{:#?}", err);
-			std::process::exit(exitcode::CONFIG);
+	if config.html.append_css_link && !config.html.custom_css.is_empty() {
+		create_css_file(&config).unwrap_or_else(|err| {
+			println!("Unable to create stylesheet! Additional info below:\n{:#?}", err);
+			exit(exitcode::CANTCREAT);
 		});
-		custom_footer.truncate(footer_len);
 	}
 
 	// This *should* never give an error, so using unwrap() here is fine.
@@ -165,7 +183,7 @@ fn main() {
 
 	files.par_iter().for_each(|fpath| {
 		println!("Parsing {}...", fpath.to_string_lossy());
-		if let Err(err) = load_parse_write(fpath, &custom_header, &custom_footer, &config) {
+		if let Err(err) = load_parse_write(fpath, &custom_html, &config) {
 			println!("Unable to parse {}! Additionl info below:\n{:#?}", fpath.to_string_lossy(), err);
 		}
 	});
