af4d48c1796349da32886586cab805c3e4a47492
Kat <10361870+katattakd@users.noreply.github.com>
2020-04-26 07:42:34 +0000

    Added example plugin

---
+++ basic-site.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+STDIN=$(cat /dev/stdin)
+# TODO: Handle writing to style.css in a thread-safe way.
+[ ! -f "style.css" ] && echo "body{font:16px/1.7 monospace;margin:auto;padding:1em 4em;max-width:64em;color:#ddd;background-color:#282a36}code,td,th{background-color:#222}code{padding:.2em .4em;font-size:95%;display:inline-block}a{color:#8ef;text-decoration:none}img{max-width:100%}blockquote{border-left:.4em solid #000;padding:0 1em;margin:0}table{border-collapse:collapse}td,th{padding:1em;border:.3em solid #000}" > "style.css"
+printf "<link rel=stylesheet href=style.css>\n"
+echo "$STDIN"
